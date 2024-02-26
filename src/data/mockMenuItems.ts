3dfe95587396125a4dca3b24350062090eecbb8f
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-02-26 19:46:02 +0200

    adding mock data, removing files

---
+++ src/data/mockMenuItems.ts
@@ -0,0 +1,16 @@
+import { MenuItem } from "../components/sections/SearchMenu";
+
+export const mockMenuItems: MenuItem[] = [
+    {
+      text: "Nintendo Switch™ - OLED Model: Mario Red Edition",
+      href: "#nintendo_switch_oled",
+    },
+    {
+      text: "Nintendo Switch",
+      href: "#nintendo_switch",
+    },
+    {
+      text: "Super Mario Bros. Wonder",
+      href: "#super_mario_bros",
+    },
+  ];
