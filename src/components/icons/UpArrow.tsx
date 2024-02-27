efb4102a3a79e7d8742b83cc5a1912307ccda4db
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-02-27 14:48:34 +0200

    adding icons, svg components

---
+++ src/components/icons/UpArrow.tsx
@@ -0,0 +1,13 @@
+export default function UpArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z" />
+    </svg>
+  );
+}
