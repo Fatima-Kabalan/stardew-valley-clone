da7cc0db473f5b685301079f2f496b5c9cdcfea4
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-02-27 14:50:55 +0200

    creating Dropdown components

---
+++ src/components/atoms/Dropdown.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { IoChevronDown, IoChevronUp } from "react-icons/io5";
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface DropdownProps {
+  options: Option[];
+  selectedOption: string;
+  setSelectedOption: Function;
+  placeholder: string;
+}
+
+export default function Dropdown({
+  options,
+  selectedOption,
+  setSelectedOption,
+  placeholder,
+}: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center gap-2 text-xl py-2 px-4 pt-4 pb-3 bg-white cursor-pointer  whitespace-nowrap"
+      >
+        <span className="text-dark_gray hover:text-red font-medium">
+          {placeholder}
+        </span>
+        {isOpen ? (
+          <IoChevronUp className="ml-2 text-dark_gray hover:text-red" />
+        ) : (
+          <IoChevronDown className="ml-2 text-dark_gray hover:text-red" />
+        )}
+      </button>
+      {isOpen && (
+        <div className="absolute top-full left-0 mt-1 bg-white w-full border border-gray-200 shadow-md rounded-md">
+          {options.map((option) => (
+            <button
+              key={option.value}
+              onClick={() => {
+                setSelectedOption(option);
+                setIsOpen(false);
+              }}
+              className="block w-full py-2 px-4 text-left hover:bg-gray-100 focus:outline-none"
+            >
+              {option.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
