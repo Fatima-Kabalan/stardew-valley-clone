becd90d50061598bd90278fb191d33624642abbc
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-02-29 17:46:37 +0200

    adding icons, svg components

--- src/components/icons/DownArrow.tsx
+++ src/components/icons/DownArrow.tsx
@@ -1,4 +1,8 @@
-export default function DownArrow() {
+interface DownArrowProps {
+  className?: string;
+}
+
+export default function DownArrow({ className }: DownArrowProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,8 +10,12 @@ export default function DownArrow() {
       height="100%"
       viewBox="0 0 24 24"
       fill="currentColor"
+      className={className}
     >
-      <path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z" />
+      <path
+        d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"
+        fill="currenColor"
+      />
     </svg>
   );
 }
--- src/components/icons/UpArrow.tsx
+++ src/components/icons/UpArrow.tsx
@@ -1,4 +1,8 @@
-export default function UpArrow() {
+interface UpArrowProps {
+  className?: string;
+}
+
+export default function UpArrow({ className }: UpArrowProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,8 +10,12 @@ export default function UpArrow() {
       height="100%"
       viewBox="0 0 24 24"
       fill="currentColor"
+      className={className}
     >
-      <path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z" />
+      <path
+        d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"
+        fill="currenColor"
+      />
     </svg>
   );
 }
