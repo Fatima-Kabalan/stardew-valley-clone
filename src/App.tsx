d938e29ae48eec2b1d68cd317410fbe67ed6907d
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-02-29 11:05:26 +0200

    adjusting the layout, routes, and adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./navigation/layout/MainLayout/MainLayout";
 
 function App() {
-  return <div className="App">HI</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
