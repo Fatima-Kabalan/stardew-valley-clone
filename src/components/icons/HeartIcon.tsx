5ae58ef0832810609328e25ea5765de5731aefcd
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-02-29 11:12:26 +0200

    creating Heart svg, icon

---
+++ src/components/icons/HeartIcon.tsx
@@ -0,0 +1,27 @@
+import Heart from "../svg/Heart";
+
+export default function HeartIcon() {
+  return (
+    <div className="hover:text-red cursor-pointer">
+      <div className="parent">
+        <div className="row one">
+          <div className="line tl"></div>
+          <div className="line"></div>
+          <div className="line tr"></div>
+        </div>
+        <div className="row two">
+          <div className="line horizontal"></div>
+          <div className="flex items-center justify-center heart ">
+            <Heart />
+          </div>
+          <div className="line horizontal"></div>
+        </div>
+        <div className="row three">
+          <div className="line bl"></div>
+          <div className="line"></div>
+          <div className="line br"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
