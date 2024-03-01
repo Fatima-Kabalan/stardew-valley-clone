b2aa56a1a0a9ee490cf26e8232dd634724c13bda
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-03-01 03:39:06 +0200

    adding icons

---
+++ src/components/icons/AddIcon.tsx
@@ -0,0 +1,18 @@
+export default function AddIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      width="100%"
+      height="100%"
+      data-testid="PlusIcon"
+      color="currentColor"
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M32.04 12h-12V0h-8v12h-12v8h12v12h8V20h12v-8z"
+      ></path>
+    </svg>
+  );
+}
