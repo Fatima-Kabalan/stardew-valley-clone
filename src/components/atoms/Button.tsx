935880af1b1589598db0fa9def1f303c0ed456f0
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-03-02 22:19:42 +0200

    adding button animation

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,16 +2,20 @@ import "animate.css";
 
 interface ButtonProps {
   text: string;
+  icon?: React.ReactNode;
   onClick?: () => void;
 }
 
-export default function Button({ text, onClick }: ButtonProps) {
+export default function Button({ text, onClick, icon }: ButtonProps) {
   return (
     <button
       className="bg-red hover:bg-rose-700 text-white  text-xl font-semibold py-4 px-6 rounded-lg shadow-lg  button-hover-animation"
       onClick={onClick}
     >
-      {text}
+      <div className="flex items-center justify-center gap-6">
+        <div className="w-8 h-8 text-white">{icon}</div>
+        <span> {text}</span>
+      </div>
     </button>
   );
 }
