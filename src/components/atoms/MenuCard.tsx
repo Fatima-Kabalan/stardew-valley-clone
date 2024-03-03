f44afe02e595ee8bbba39813f820096c544a64b0
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-03-03 16:35:27 +0200

    fixing the structure of MenuCard

--- src/components/atoms/MenuCard.tsx
+++ src/components/atoms/MenuCard.tsx
@@ -1,21 +1,25 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
-interface MenuCardProps {
+export interface Card {
+  icon: ReactNode;
   text: string;
   to: string;
-  icon: ReactNode;
 }
 
-export default function MenuCard({ text, icon, to }: MenuCardProps) {
+interface MenuCardProps {
+  card: Card;
+}
+
+export default function MenuCard({ card }: MenuCardProps) {
   return (
-    <Link to={to}>
+    <Link to={card.to}>
       <div className="flex flex-col items-center">
         <div className="flex flex-col items-center justify-center border border-solid border-gray-300 rounded-2xl gap-2 w-36 p-2 transition duration-300 ease-in-out hover:bg-red hover:text-white">
-          <span className="flex justify-center w-16">{icon}</span>
+          <span className="flex justify-center w-16">{card.icon}</span>
         </div>
         <h1 className="text-center text-sm text-gray-500 hover:text-red  md:text-lg">
-          {text}
+          {card.text}
         </h1>
       </div>
     </Link>
